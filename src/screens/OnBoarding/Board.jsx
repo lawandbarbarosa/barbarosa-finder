5bb1728e0d7054270eaa05573472e0b34b625f4e
lawand barbarosa <lawandata14@gmail.com>
2023-05-30 22:14:52 +0300

    initial commit

---
+++ src/screens/OnBoarding/Board.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Board() {
+  return (
+    <View>
+      <Text>Board</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
